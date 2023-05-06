f17bc7540aef2cc02a1ca29e771c194bd9c01413
Yash <yash.glazonoid@gmail.com>
2023-05-06 17:27:58 +0530

    task option edit in RejectedAdapter

--- app/src/main/java/com/example/notificationmanager/adapter/RejectedAdapter.java
+++ app/src/main/java/com/example/notificationmanager/adapter/RejectedAdapter.java
@@ -19,6 +19,8 @@ import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -29,6 +31,7 @@ import com.google.firebase.firestore.QuerySnapshot;
 import org.json.JSONArray;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class RejectedAdapter extends FirestoreRecyclerAdapter<NotificationModel, RejectedAdapter.RejectedViewHolder> {
 
@@ -50,29 +53,78 @@ public class RejectedAdapter extends FirestoreRecyclerAdapter<NotificationModel,
             @Override
             public void onClick(View v) {
                 JSONArray array = new JSONArray();
-                FirebaseFirestore db = FirebaseFirestore.getInstance();
-                CollectionReference userCollection = db.collection("Users");
+                CollectionReference usersCollection = FirebaseFirestore.getInstance().collection("Users");
+                Query query = usersCollection;
 
-                Query query = userCollection;
-
+                ArrayList<String> workLocations = model.getLocations();
                 ArrayList<String> departmentTypes = model.getDepartments();
                 ArrayList<String> genders = model.getGenders();
-                ArrayList<String> workLocations = model.getLocations();
 
-                Log.d("RejectedAdapter", "Before Query: " + query.toString());
+                ArrayList<Task<QuerySnapshot>> tasks = new ArrayList<>();
 
-                if (Boolean.TRUE.equals(model.getQuery().get("Location"))){
-                    Log.d("ModelQuery","Location "+model.getQuery().get("Location").toString());
-                    query = query.whereIn("WorkLocation",workLocations);
+// Add each query task to the tasks list
+                if (workLocations != null && !workLocations.isEmpty()) {
+                    tasks.add(usersCollection.whereIn("WorkLocation", workLocations).get());
                 }
-                if (Boolean.TRUE.equals(model.getQuery().get("Department"))){
-                    Log.d("ModelQuery","Department "+model.getQuery().get("Department").toString());
-                    query = query.whereArrayContainsAny("DepartmentTypes",departmentTypes);
+                if (departmentTypes != null && !departmentTypes.isEmpty()) {
+                    tasks.add(usersCollection.whereArrayContainsAny("DepartmentTypes", departmentTypes).get());
                 }
-                if (Boolean.TRUE.equals(model.getQuery().get("Gender"))){
-                    Log.d("ModelQuery","Gender "+model.getQuery().get("Gender").toString());
-                    query = query.whereIn("Gender",genders);
+                if (genders != null && !genders.isEmpty()) {
+                    tasks.add(usersCollection.whereIn("Gender", genders).get());
                 }
+
+// Wait for all tasks to complete before executing any further code
+                Tasks.whenAllSuccess(tasks).addOnSuccessListener(new OnSuccessListener<List<Object>>() {
+                    @Override
+                    public void onSuccess(List<Object> objects) {
+                        JSONArray array = new JSONArray();
+                        for (Object object : objects) {
+                            QuerySnapshot querySnapshot = (QuerySnapshot) object;
+                            for (QueryDocumentSnapshot document : querySnapshot) {
+                                User user = document.toObject(User.class);
+                                array.put(user.getFCMToken());
+                            }
+                        }
+                        Log.d("fcm", String.valueOf(array));
+                        FCMSend.pushNotificationMultiple(v.getContext(), array, model.getTitle(), model.getDescription(), "please check");
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d("Firestore", "Error getting documents: ", e);
+                    }
+                });
+
+
+//                query.get().addOnCompleteListener(task -> {
+//                    if (task.isSuccessful()) {
+//                        for (QueryDocumentSnapshot document : task.getResult()) {
+//                            User user = document.toObject(User.class);
+//                            array.put(user.getFCMToken());
+//                        }
+//                        Log.d("fcm",String.valueOf(array));
+//                        FCMSend.pushNotificationMultiple(v.getContext(), array, model.getTitle(), model.getDescription(), "please check");
+//                    } else {
+//                        Log.d("Firestore", "Error getting documents: ", task.getException());
+//                    }
+//                });
+
+
+//                if (Boolean.TRUE.equals(model.getQuery().get("Location"))){
+//                    Log.d("ModelQuery","Location "+model.getQuery().get("Location").toString());
+//                    query = query.whereIn("WorkLocation",workLocations);
+//                    querry++;
+//                }
+//                if (Boolean.TRUE.equals(model.getQuery().get("Department"))){
+//                    Log.d("ModelQuery","Department "+model.getQuery().get("Department").toString());
+//                    query = query.whereArrayContainsAny("DepartmentTypes",departmentTypes);
+//                    querry++;
+//                }
+//                if (Boolean.TRUE.equals(model.getQuery().get("Gender"))){
+//                    Log.d("ModelQuery","Gender "+model.getQuery().get("Gender").toString());
+//                    query = query.whereIn("Gender",genders);
+//                    querry++;
+//                }
 
 
 //                if (Boolean.TRUE.equals(model.getQuery().get("Location"))){
@@ -88,44 +140,100 @@ public class RejectedAdapter extends FirestoreRecyclerAdapter<NotificationModel,
 //                    query=query.whereIn("Gender",genders);
 //                }
 
-                Log.d("RejectedAdapter", "After Query: " + query.toString());
-
-//                Log.d("ModelQuery",String.valueOf(userCollection));
-
-//                if (!model.getDepartments().isEmpty()){
-//                    query.whereIn("DepartmentTypes",departmentTypes);
-//                }
-//                if (!model.getLocations().isEmpty()){
-//                    query.whereIn("WorkLocation",workLocations);
-//                }
-//                if (!model.getLocations().isEmpty()){
-//                    query.whereIn("WorkLocation",workLocations);
-//                }
+//                Log.d("RejectedAdapter", "After Query: " + query.toString());
 
 
-                query.get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
-                    @Override
-                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
-                        Log.d("Insidethis","inside the success listener");
-                        // Iterate through the query results
-                        for (QueryDocumentSnapshot document : queryDocumentSnapshots) {
-                            // Get the document data as a User object
-                            User user = document.toObject(User.class);
-                            array.put(user.getFCMToken());
-                            Log.d("userFcmToken",user.getFCMToken());
-                        }
-                        FCMSend.pushNotificationMultiple(v.getContext(),array,model.getTitle(),model.getDescription(),"please check"  );
-                        Log.d("fcmArray",String.valueOf(array));
-
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.d("Insidethis","inside the faliure listener");
-
-                        Log.e("TAG", "Error getting users: ", e);
-                    }
-                });
+//                query.get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+//                    @Override
+//                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+//                        Log.d("Insidethis","inside the success listener");
+//                        for (QueryDocumentSnapshot document : queryDocumentSnapshots) {
+//                            // Get the document data as a User object
+//                            User user = document.toObject(User.class);
+//
+//                            array.put(user.getFCMToken());
+////                            if (Boolean.TRUE.equals(model.getQuery().get("Location"))) {
+////                                if (workLocations.contains(user.getWorkLocation())) {
+////                                    Log.d("ModelQuery","Location "+model.getQuery().get("Location").toString());
+////
+////                                    array.put(user.getFCMToken());
+////                                }
+////                            }
+////                            if (Boolean.TRUE.equals(model.getQuery().get("Department"))) {
+////                                if (departmentTypes.containsAll(user.getDepartmentTypes())) {
+////                                    Log.d("ModelQuery","Department "+model.getQuery().get("Department").toString());
+////                                    array.put(user.getFCMToken());
+////                                }
+////                            }
+////                            if (Boolean.TRUE.equals(model.getQuery().get("Gender"))) {
+////                                if (genders.contains(user.getGender())) {
+////                                    Log.d("ModelQuery","Gender "+model.getQuery().get("Gender").toString());
+////                                    array.put(user.getFCMToken());
+////                                }
+////                            }
+//                        }
+//                        FCMSend.pushNotificationMultiple(v.getContext(), array, model.getTitle(), model.getDescription(), "please check");
+//                        Log.d("fcmArray", String.valueOf(array));
+//
+//                        // Iterate through the query results
+////                        for (QueryDocumentSnapshot document : queryDocumentSnapshots) {
+////                            // Get the document data as a User object
+////                            User user = document.toObject(User.class);
+////
+////                            if (Boolean.TRUE.equals(model.getQuery().get("Location"))){
+////                                Log.d("ModelQuery","Location "+model.getQuery().get("Location").toString());
+////                                array.put(user.getFCMToken());
+////                            }
+////                            if (Boolean.TRUE.equals(model.getQuery().get("Department"))){
+////                                Log.d("ModelQuery","Department "+model.getQuery().get("Department").toString());
+////
+////                                for (int i = 0; i < array.length(); i++) {
+////                                    try {
+////                                        Log.d("fcm","inside the try department");
+////                                        if (!array.getString(i).equals(user.getFCMToken())) {
+////                                            array.put(user.getFCMToken());
+////                                            break;
+////                                        }
+////                                    } catch (JSONException e) {
+////                                        throw new RuntimeException(e);
+////                                    }
+////                                }
+//////                                array.put(user.getFCMToken());
+////
+////                            }
+////                            if (Boolean.TRUE.equals(model.getQuery().get("Gender"))){
+////                                Log.d("ModelQuery","Gender "+model.getQuery().get("Gender").toString());
+////                                for (int i = 0; i < array.length(); i++) {
+////                                    try {
+////                                        Log.d("fcm","inside the try gender");
+////
+////                                        if (!array.getString(i).equals(user.getFCMToken())) {
+////                                            array.put(user.getFCMToken());
+////                                            break;
+////                                        }
+////                                    } catch (JSONException e) {
+////                                        throw new RuntimeException(e);
+////                                    }
+////                                }
+//////                                array.put(user.getFCMToken());
+////
+////                            }
+////
+//////                            array.put(user.getFCMToken());
+//////                            Log.d("userFcmToken",user.getFCMToken());
+////                        }
+////                        FCMSend.pushNotificationMultiple(v.getContext(),array,model.getTitle(),model.getDescription(),"please check"  );
+////                        Log.d("fcmArray",String.valueOf(array));
+//
+//                    }
+//                }).addOnFailureListener(new OnFailureListener() {
+//                    @Override
+//                    public void onFailure(@NonNull Exception e) {
+//                        Log.d("Insidethis","inside the faliure listener");
+//
+//                        Log.e("TAG", "Error getting users: ", e);
+//                    }
+//                });
 
 //                array.put("cEzbCvUXQgeAqhJTg03Iwj:APA91bE4HZlGsmf95P7Kthvm6t3zlPwzqbIPk0jKGAN2Gm65dI5meM2tNRoQ9IunrjwsxoSaJGBIewGiETLDEu7bA8qMUJ4QCDn2VXZBeFdbP02u-A6DOhZxOipTIvL0XeL8-OFyHvxv");
 
